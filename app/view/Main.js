657464adc02943aeabcc88aeca65397260cff0dc
titan-kun <theanh_phan1990@yahoo.com>
2015-04-14 10:20:18 +1400

    Test github

--- app/view/Main.js
+++ app/view/Main.js
@@ -22,11 +22,7 @@ Ext.define('FahasaECom.view.Main', {
                     title: 'Welcome to Sencha Touch 2'
                 },
 
-                html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
-                ].join("")
+                html: "The Anh"
             },
             {
                 title: 'Get Started',
